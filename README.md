c4ce26376504689c729dd9295005a3f5cbb80d8b
Simon Jia <bizsimon@gmail.com>
2022-07-14 11:25:53 -0400

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# literally-profanity
+# Literally Media Profanity List
+
+## Installation
+* run `npm install` or `yarn install`
+
+## Usage
+* Edit `literally-words.txt` to add any new profanity words
+* Run `node index.js` to update `final-list.json`
+* Make sure to commit changes back to Github with updated `literally-words.txt` and `final-list.json`
+
+## Sources
+All sources are combined and deduped before `final-list.json` is generated
+
+3rd Party sources are defined under `sources.txt` and currently support `txt` and `json` formats
+* `literally-words.txt`
+* https://github.com/coffee-and-fun/google-profanity-words
+* https://github.com/zacanger/profane-words
---
+++ index.js
@@ -0,0 +1,60 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+const colors = require('colors');
+var pjson = require('./package.json');
+
+console.log(`Literally Media Profanity List - ${pjson.version}`);
+
+let thirdPartySources = fs.readFileSync('./sources.txt', {encoding:'utf8', flag:'r'});
+thirdPartySources = thirdPartySources.split("\n");
+
+let literallySource = fs.readFileSync('./literally-words.txt', {encoding:'utf8', flag:'r'});
+if (literallySource.trim() != '') {
+    literallySource = literallySource.trim().split("\n");
+} else {
+    literallySource = [];
+}
+
+console.log(`Fetched Source: ./literally-words.txt - Found ${literallySource.length} words`);
+
+let list = literallySource;
+
+async function main() {
+    
+
+    await Promise.all(thirdPartySources.map(source => {
+        let ext = path.extname(source);
+
+        return fetch(source)
+            .then(res => {
+                if (ext == '.json') {
+                    return res.json();
+                }
+                return res.text();
+            })
+            .then(body => {
+                if (ext != '.json') {
+                    body = body.split("\n");
+                }
+
+                console.log(`Fetched Source: ${source} - Found ${body.length} words`);
+    
+                list = list.concat(body);
+            });
+    }))
+        .then(() => {
+            const finalList = [ ...new Set(list) ];
+
+            fs.writeFileSync('final-list.json', JSON.stringify(finalList));
+
+            console.log(`Total entries after deduping: ${finalList.length} @ final-list.json`.green);
+        });
+}
+
+main();
+
+
+
+
+
